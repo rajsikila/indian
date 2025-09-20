9b2912fe3a9cd24a4cce08e135be586677490a2a
rajsikila <rajasvvsns@gmail.com>
2025-09-20 17:09:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,74 @@
-// Initialize the map
-var map = L.map('map').setView([20.5937, 78.9629], 5); // Centered on India
+// Initialize the map (center on India)
+var map = L.map('map').setView([22.9734, 78.6569], 5);
 
-// Set up the tile layer (map background)
+// Add OpenStreetMap tiles
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  maxZoom: 18,
+  attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-// Locations with historical and cultural relevance to Indian art
+// Locations data (including temples and new locations)
 var locations = [
-    {
-        name: 'Mumbai',
-        lat: 19.0760,
-        lon: 72.8777,
-        description: 'Mumbai is home to contemporary Indian art movements, galleries, and museums, such as the Chhatrapati Shivaji Maharaj Vastu Sangrahalaya.',
-        img: 'path-to-image.jpg'
-    },
-    {
-        name: 'Kolkata',
-        lat: 22.5726,
-        lon: 88.3639,
-        description: 'Kolkata is known for its rich history in the Bengal School of Art, with artists like Abanindranath Tagore making a significant impact.',
-        img: 'path-to-image.jpg'
-    },
-    {
-        name: 'Chennai',
-        lat: 13.0827,
-        lon: 80.2707,
-        description: 'Chennai is a center for traditional South Indian art, including classical dance, sculpture, and Tamil literature.',
-        img: 'path-to-image.jpg'
-    },
-    {
-        name: 'Jaipur',
-        lat: 26.9124,
-        lon: 75.7873,
-        description: 'Jaipur is famous for its miniature painting style and the distinct Rajasthani art forms that have influenced Indian art history.',
-        img: 'path-to-image.jpg'
-    },
-    {
-        name: 'Bhopal',
-        lat: 23.2599,
-        lon: 77.4126,
-        description: 'Bhopal is known for its traditional Madhya Pradesh art forms, including Gond paintings and crafts.',
-        img: 'path-to-image.jpg'
-    }
+  {
+    name: "Ajanta Caves",
+    coords: [20.5519, 75.7033],
+    info: "<b>Ajanta Caves</b><br>2nd BCE–6th CE<br>Buddhist mural paintings"
+  },
+  {
+    name: "Ellora Caves",
+    coords: [20.0268, 75.1793],
+    info: "<b>Ellora Caves</b><br>6th–10th CE<br>Rock-cut architecture and sculpture"
+  },
+  {
+    name: "Sanchi Stupa",
+    coords: [23.4793, 77.7398],
+    info: "<b>Sanchi Stupa</b><br>3rd BCE<br>Buddhist architecture"
+  },
+  {
+    name: "Khajuraho Temples",
+    coords: [24.8318, 79.9199],
+    info: "<b>Khajuraho Temples</b><br>10th CE<br>Intricate temple carvings and sculptures"
+  },
+  {
+    name: "Tanjore (Thanjavur)",
+    coords: [10.7870, 79.1378],
+    info: "<b>Tanjore</b><br>Famous for Tanjore paintings with gold leaf work"
+  },
+  {
+    name: "Shantiniketan",
+    coords: [23.6815, 87.6850],
+    info: "<b>Shantiniketan</b><br>Founded by Rabindranath Tagore<br>Center of Bengal School of Art"
+  },
+  // New Locations (Museums, Galleries, etc.)
+  {
+    name: "National Gallery of Modern Art",
+    coords: [28.6185, 77.2090],
+    info: "<b>National Gallery of Modern Art</b><br>Located in New Delhi<br>Holds Indian modern art collections"
+  },
+  {
+    name: "Chhatrapati Shivaji Maharaj Vastu Sangrahalaya",
+    coords: [18.9230, 72.8335],
+    info: "<b>Chhatrapati Shivaji Maharaj Vastu Sangrahalaya</b><br>Located in Mumbai<br>Art, culture, and history museum"
+  },
+  {
+    name: "Delhi Haat",
+    coords: [28.5355, 77.2456],
+    info: "<b>Delhi Haat</b><br>A marketplace and cultural center<br>Showcases traditional Indian crafts and arts"
+  },
+  {
+    name: "Rajasthan State Museum",
+    coords: [26.9152, 75.8553],
+    info: "<b>Rajasthan State Museum</b><br>Located in Jaipur<br>Displays Rajasthani art and cultural heritage"
+  },
+  {
+    name: "The Kala Ghoda Arts Festival",
+    coords: [18.9353, 72.8258],
+    info: "<b>The Kala Ghoda Arts Festival</b><br>Located in Mumbai<br>Annual festival celebrating Indian arts, crafts, and culture"
+  }
 ];
 
-// Add markers for each location with popup content
-locations.forEach(function(location) {
-    var marker = L.marker([location.lat, location.lon]).addTo(map);
-    var popupContent = `
-        <div class="popup-content">
-            <h3>${location.name}</h3>
-            <img src="${location.img}" alt="${location.name}" style="width: 100%; height: auto;">
-            <p>${location.description}</p>
-        </div>
-    `;
-    marker.bindPopup(popupContent);
+// Add markers
+locations.forEach(loc => {
+  L.marker(loc.coords).addTo(map)
+    .bindPopup(loc.info);
 });
-
-
-
